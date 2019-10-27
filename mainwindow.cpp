01b6c39c388dc3760aeb0e1766cab7cb13170d64
Limberjack <you@example.com>
2019-10-27 17:59:21 +0300

    homework 1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -21,7 +21,38 @@ class User{
     int age; // private
     QString name; //private
     static int total_count;
+    static std::vector<User> users_;
+
+
 public:
+
+    static void addStudent(User user){
+        users_.push_back(user);
+    }
+    static void removeStudentByIndex(int index){
+        std::vector<User>::iterator iterator_ = users_.begin();
+        if(index < users_.size())
+        users_.erase(iterator_ + index);
+        users_.shrink_to_fit();
+    }
+    static bool removeByName(QString studentName){
+        std::vector<User>::iterator iterator_= users_.begin();
+        while(iterator_ != users_.end()){
+            if( (*iterator_).getName() == studentName ){
+                users_.erase(iterator_);
+                users_.shrink_to_fit();
+                return true;
+            }
+            iterator_++;
+        }
+        return false;
+    }
+
+   static  int getStudentListSize(){
+        return users_.size();
+    }
+
+
     explicit User(QString name, int age=18){
         if(age>=18){
             this->age=age;
@@ -40,7 +71,7 @@ public:
     static int getTotalCount(){
         return total_count;
     }
-    static std::vector<User> users_;
+
 };
 
 int User::total_count = 0;
@@ -48,7 +79,24 @@ std::vector<User> User::users_;
 
 void MainWindow::on_submitPushButton_clicked()
 {
-    qDebug() << "User clicked on submit button";
+    User a("a"), b("b"), c("c");
+    User::addStudent(a);
+    qDebug() << User::getStudentListSize();
+    User::addStudent(b);
+    qDebug() << User::getStudentListSize();
+    User::addStudent(c);
+
+    qDebug() << User::getStudentListSize();
+
+    User::removeByName("c");
+    qDebug() << User::getStudentListSize();
+
+    User::removeStudentByIndex(0);
+    qDebug() << User::getStudentListSize();
+
+
+
+    /*qDebug() << "User clicked on submit button";
     // ui->nameLineEdit->setText("Aufar");
     User student(ui->nameLineEdit->text(), ui->ageLineEdit->text().toInt());
 
@@ -65,4 +113,5 @@ void MainWindow::on_submitPushButton_clicked()
     qDebug() << "First student name ([0]):" << User::users_[0].getName();
     qDebug() << "First student name (at):" << User::users_.at(0).getName();
     qDebug() << "------------------------";
+*/
 }
