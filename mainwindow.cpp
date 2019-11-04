9b88c384d89259b476901b8c38dda856167cbecd
NotArtyom <smart.artyom@gmail.com>
2019-11-04 21:58:53 +0300

    This gonna be the last one. Corrected everything according to comments

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,9 +18,11 @@ MainWindow::~MainWindow()
 
 
 class User{
-    int age; // private
-    QString name; //private
-    static int total_count;
+private:
+    int age;
+    QString name;
+    //static int total_count;
+    static std::vector<User> users_;
 public:
     explicit User(QString name, int age=18){
         if(age>=18){
@@ -29,7 +31,6 @@ public:
             this->age=18;
         }
         this->name=name;
-        total_count++;
     }
     QString getName(){
         return name;
@@ -37,64 +38,45 @@ public:
     int getAge(){
         return age;
     }
-    static int getTotalCount(){
-        return total_count;
-    }
-    static std::vector<User> users_;
     
-    void addStudent(QString name, int age){
-        User::users_.push_back(*new User(name,age));
+    static void addStudent(User user){
+        users_.push_back(user);
     }
     
-    static long itsAFinalCoundown(){
-        return User::users_.size();
+    static long TotalCount(){
+        return users_.size();
     }
     
     static void delStudent(long index){
-        for (long i = index; i < User::users_.size()-1;i++);
-        User::users_.at(index) = User::users_.at(index+1);
-        User::users_.erase(User::users_.begin()+ User::users_.size()-1);
+        users_.erase(users_.begin()+ index);
     }
     
     static void delStudent(QString name){
         long index = -1;
-        for (long i = 0 ; i < User::users_.size();i++)
+        for (long i = 0 ; i < users_.size();i++)
         {
-            if (User::users_.at(i).getName() == name)
+            if (users_.at(i).getName() == name)
             index = i;
         }
         if (index != -1){
-            for (long i = index; i < User::users_.size()-1;i++);
-            User::users_.at(index) = User::users_.at(index+1);
-            User::users_.erase(User::users_.begin()+ User::users_.size()-1);
+            users_.erase(users_.begin()+ index);
             
+        } else{
+            qDebug() << "User is not found";
         }
         }
 };
 
-int User::total_count = 0;
 std::vector<User> User::users_;
-
-
 
 void MainWindow::on_submitPushButton_clicked()
 {
     qDebug() << "User clicked on submit button";
     // ui->nameLineEdit->setText("Aufar");
     User student(ui->nameLineEdit->text(), ui->ageLineEdit->text().toInt());
-
-    QMessageBox msg(QMessageBox::Information,"New student arrived!",
-                    "Hello "+ student.getName() + "!");
-    // qDebug() << "Name:" << student.getName();
-    // qDebug() << "Age:" << student.getAge();
-    qDebug() << msg.exec();
-    qDebug() << User::getTotalCount();
-    User::users_.push_back(student);
-    //qDebug() << "Last student age:" << User::users_.end()->getAge();
-    //qDebug() << "Last student name:" << User::users_.end()->getName();
-    qDebug() << "Total count:" << User::users_.size();
-    qDebug() << "First student name ([0]):" << User::users_[0].getName();
-    qDebug() << "First student name (at):" << User::users_.at(0).getName();
+    User::addStudent(student);
+    User::delStudent(0);
+    qDebug() << User::TotalCount();
     qDebug() << "------------------------";
 
 }
